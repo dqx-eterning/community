2498ee08bcd0735d7ee75859495a6d6adef42d7a
dqx_eterning <1986813958@qq.com>
2020-09-10 17:35:56 +0800

    init repo

---
+++ src/main/java/dqx/community/controller/HelloControaller.java
@@ -0,0 +1,16 @@
+package dqx.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloControaller {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
